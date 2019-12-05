fe8c47f62b9ccdc1f9b65d62759d3dab29dadcf3
Nik Stone <34091480+NikStone228@users.noreply.github.com>
2019-12-05 13:30:10 +0400

    Update DescriptionTableViewCell.swift

--- Altarix API Homework/Sources/Views/DescriptionTableViewCell.swift
+++ Altarix API Homework/Sources/Views/DescriptionTableViewCell.swift
@@ -1,10 +1,4 @@
-//
-//  DescriptionTableViewCell.swift
-//  Altarix API Homework
-//
-//  Created by Sergey Popyvanov on 28.11.2019.
-//  Copyright © 2019 Sergey Popyvanov. All rights reserved.
-//
+
 
 import UIKit
 
